135a4ba9572982d139e909191bbea542bd7b4be7
Nachokeso <167124110+IgnacioBerrios@users.noreply.github.com>
2024-11-06 19:47:21 -0300

    Update pagina_web.py

--- pagina_web.py
+++ pagina_web.py
@@ -3,14 +3,14 @@ import random
 import os        #nacho acuerdate que esto sirve para trabajar con rutas :D
 from PIL import Image #para usar imagenes perrito
 
-RUTA_BASE = "tiernos"  #elegimos la ruta para los animales to lindos, to tiernos
+la_ruta_de_las_cuestiones = "tiernos"  #elegimos la ruta para los animales to lindos, to tiernos
 CARPETAS = {
-    "mapache": os.path.join(RUTA_BASE, "mapache"),
-    "zorrito": os.path.join(RUTA_BASE, "zorrito"),
-    "pandita rojo": os.path.join(RUTA_BASE, "pandita rojo")
+    "mapache": os.path.join(la_ruta_de_las_cuestiones, "mapache"),
+    "zorrito": os.path.join(la_ruta_de_las_cuestiones, "zorrito"),
+    "pandita rojo": os.path.join(la_ruta_de_las_cuestiones, "pandita rojo")
 }
 # ahora la playlis de todo para musicalisar la cosa
-CANCIONES = {
+musiquita = {
     "Canción más tierna": "cancion_mas_tierna.mp3",    #opening Death Note
     "Canción Miku": "cancion_Miku.mp3",                #esa de miku que dice ponpiponpipnopopipo
     "Caramel Dancing": "caramel_dancing.mp3"           #Caramel Dancig
@@ -22,28 +22,24 @@ def obtener_imagen(ruta_carpeta):
         imagen = random.choice(imagenes)
         return Image.open(os.path.join(ruta_carpeta, imagen))
     return None
-
 #barrita para decorar y saber tu opinion apesar de que no ira a ningun lugar y todo morira
 st.title("Para ti, qué animal es más lindo?")
 with st.sidebar:
-    st.header("Tu que cre'")
+    st.header("Tu que crees")
     respuesta = st.text_input("Cuál de estos animales te parece el más lindo y por qué? explayece")
     if st.button("Enviar"):
         if respuesta:
             st.sidebar.write("Gracias por tu opinion ☺️☺️☺️")
         else:
             st.sidebar.write("Por favor, pongase serio y envie una opinion real.....")
-
     if st.button("click aqui ⬇️⬇️"):                #pa trolear
         if respuesta:
             st.sidebar.write("Gracias por clicklar 👍👍")
-
 #music pal cuerpo
     st.header("elija una cancion (la mejor es la mas tierna pero no es tan tierna para se sincero)")
-    cancion_seleccionada = st.selectbox("Elige una canción para escuchar:", list(CANCIONES.keys()))
+    cancion_seleccionada = st.selectbox("Elige una canción para escuchar:", list(musiquita.keys()))
     if cancion_seleccionada:
-        st.audio(CANCIONES[cancion_seleccionada], format="audio/mp3")
-
+        st.audio(musiquita[cancion_seleccionada], format="audio/mp3")
 #mostrando la seleccion de animales
 if "mostrar_seleccion" not in st.session_state:
     st.session_state.mostrar_seleccion = True
@@ -73,4 +69,3 @@ if not st.session_state.mostrar_seleccion:
     #para volver (no se que mas decir creo que se explica solo)
     if st.button("volver"):
         st.session_state.mostrar_seleccion = True
-
